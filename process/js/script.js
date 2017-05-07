654ae9838a4c8856ca186b9f41802d82b5edcdb2
Ray Villalobos <ray@planetoftheweb.com>
2017-05-06 20:41:54 -0400

    start phase

--- process/js/script.js
+++ process/js/script.js
@@ -12,17 +12,8 @@ var colors = [
 function makeBoxes(howMany) {
   var colorAmt = colors.length;
   var currColor = 0;
-  var myElement;
-  var myNode = document.querySelector('.boxes');
 
   for (var i = 0; i < howMany; i++) {
-    myElement = document.createElement('div');
-    myElement.className = 'box';
-    myElement.style = 'background-color: ' + colors[currColor];
-    myNode.appendChild(myElement);
-    myNode.addEventListener('click', function(e) {
-      e.target.parentNode.removeChild(e.target);
-    }, false);
 
     if (currColor === colorAmt-1) {
       currColor = 0;
