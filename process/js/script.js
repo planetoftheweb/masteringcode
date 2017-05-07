25edd839b03b74b480389bd5e26ffb8506866b07
Ray Villalobos <ray@planetoftheweb.com>
2017-05-06 18:33:10 -0700

    DOM Manipulation

--- process/js/script.js
+++ process/js/script.js
@@ -12,8 +12,15 @@ var colors = [
 function makeBoxes(howMany) {
   var colorAmt = colors.length;
   var currColor = 0;
+  var myElement;
+  var myNode = document.querySelector('.boxes');
 
   for (var i = 0; i < howMany; i++) {
+
+    myElement = document.createElement('div');
+    myElement.className = 'box';
+    myElement.style = 'background-color: ' + colors[currColor];
+    myNode.appendChild(myElement);
 
     if (currColor === colorAmt-1) {
       currColor = 0;
@@ -21,6 +28,11 @@ function makeBoxes(howMany) {
       currColor++;
     }
   }
+
+  myNode.addEventListener('click', function(e) {
+    e.target.parentNode.removeChild(e.target);
+  }, false);
+
 }
 
-makeBoxes(20);
+makeBoxes(10);
