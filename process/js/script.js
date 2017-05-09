49a3b4e4a2eb92792a63baecbb3be63c22ba88b0
Ray Villalobos <ray@planetoftheweb.com>
2017-05-08 19:02:31 -0700

    callbacks:

---
+++ process/js/script.js
@@ -0,0 +1,7 @@
+var tips = document.querySelectorAll('.hastip');
+
+for(var i = 0; i < tips.length; i++) {
+  tips[i].addEventListener('click', function(e) {
+    e.target.querySelector('.tooltip').classList.toggle('active');
+  }, false);
+}
