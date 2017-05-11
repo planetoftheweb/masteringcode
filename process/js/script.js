d787c3618b52eabbe7b1f5f1286303fddd3b0bae
Ray Villalobos <ray@planetoftheweb.com>
2017-05-10 20:00:10 -0400

    constructor pattern

--- process/js/script.js
+++ process/js/script.js
@@ -1,35 +1,20 @@
-(function(howMany, colors) {
+function Hamburger(nodeName) {
+	var myNode = document.querySelector(nodeName + ' .hamburger');
+	return {
+		activate: function() {
+				myNode.addEventListener('click', function(e) {
+				myNode.parentNode.querySelector('.navbar').classList.toggle('hidden');
+			}, false);	
+		},
+		hide: function() {
+			myNode.parentNode.querySelector('.navbar').classList.add('hidden');
+		}
+	} //return
+} // Hamburger
 
-  var colorAmt = colors.length;
-  var currColor = 0;
-  var myElement;
-  var myNode = document.querySelector('.boxes');
+var topMenu = new Hamburger('#topMenu');
+topMenu.activate();
 
-  for (var i = 0; i < howMany; i++) {
-
-    myElement = document.createElement('div');
-    myElement.className = 'box';
-    myElement.style = 'background-color: ' + colors[currColor];
-    myNode.appendChild(myElement);
-
-    if (currColor === colorAmt-1) {
-      currColor = 0;
-    } else {
-      currColor++;
-    }
-  }
-
-  myNode.addEventListener('click', function(e) {
-    e.target.parentNode.removeChild(e.target);
-  }, false);
-
-})(20, [
-  '#C94C24', //orange
-  '#268BD2', //blue
-  '#C4226F', //pink
-  '#859835', // lime
-  '#6D73C2', // purple
-  '#37A198', // green
-  '#DA3637', //red
-  '#F0AD4E' //yellow
-]);
+var bottomMenu = new Hamburger('#bottomMenu');
+bottomMenu.activate();
+bottomMenu.hide();
