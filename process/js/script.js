6a26f13da5ee7135c986983105c321e1eb3f01b9
Ray Villalobos <ray@planetoftheweb.com>
2017-06-26 21:28:27 -0400

    EP017b

--- process/js/script.js
+++ process/js/script.js
@@ -1,7 +1,5 @@
-var data, DOMNode;
+var data;
 var request = new XMLHttpRequest();
-
-DOMNode = document.querySelector('.artists');
 
 request.open('GET', 'js/data.json');
 
@@ -11,16 +9,8 @@ request.onreadystatechange = function() {
     request.readyState === 4
   ) {
     data = JSON.parse(request.responseText);
-
-    for (var item in data.artists) {
-      if (data.artists.hasOwnProperty(item)) {
-        var listItem = document.createElement('div');
-        listItem.className = 'artist';
-        listItem.innerHTML = '<h4>' + data.artists[item].name + '</h4>'
-          + '<p>' + data.artists[item].bio + '</p>';
-        DOMNode.appendChild(listItem);
-      }
-    }
+    console.log(data);
+    console.log(request);
   }
 }
 
