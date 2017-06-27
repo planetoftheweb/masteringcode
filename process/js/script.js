bf8ce7a434f0912dc585f37b7e3ac7b21139023d
Ray Villalobos <ray@planetoftheweb.com>
2017-06-26 20:39:35 -0400

    changes to script.js

---
+++ process/js/script.js
@@ -0,0 +1,17 @@
+var data;
+var request = new XMLHttpRequest();
+
+request.open('GET', 'js/data.json');
+
+request.onreadystatechange = function() {
+  if(
+    request.status === 200 &&
+    request.readyState === 4
+  ) {
+    data = JSON.parse(request.responseText);
+    console.log(data);
+    console.log(request);
+  }
+}
+
+request.send();
