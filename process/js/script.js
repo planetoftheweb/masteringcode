90b8e30f94053331c25d6a1c58bd30b1ddfcb9ae
Ray Villalobos <ray@planetoftheweb.com>
2017-07-18 11:16:41 -0700

    EP018e

--- process/js/script.js
+++ process/js/script.js
@@ -6,6 +6,10 @@ request.open('GET', 'js/data.json');
 request.onreadystatechange = function () {
   if (request.status === 200 && request.readyState === 4) {
     data = JSON.parse(request.responseText);
+
+    result = new EJS({url: 'js/template.ejs'}).render(data.artists);
+    document.querySelector('.artists').innerHTML = result;
+
   }
 };
 
