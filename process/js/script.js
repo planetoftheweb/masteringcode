ed5d71611850bbd051094a74ba355522f827a6c8
Ray Villalobos <ray@planetoftheweb.com>
2017-07-21 19:34:21 -0400

    prepped EP024b

--- process/js/script.js
+++
@@ -1,7 +0,0 @@
-var highlight = document.querySelectorAll('[data-highlight-click]');
-
-highlight.forEach(function(item) {
-  item.addEventListener('click', function() {
-    item.style.backgroundColor = 'blue';
-  }, false);
-}, this);
