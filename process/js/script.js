6066fd139256292d814999a0edf7f873afee71d0
Ray Villalobos <ray@planetoftheweb.com>
2017-07-25 15:34:18 -0700

    EP025e

--- process/js/script.js
+++ process/js/script.js
@@ -1,14 +1,15 @@
-var howMany = 12;
+// var howMany = 12;
+/// createBoxes(howMany);
 
-function createBoxes(qty = 4) {
+(function createBoxes(qty = 4) {
   var myBoxes = document.querySelector('.boxes');  
+  // console.log(myNode);
+  // console.log(i);
 
-  for (var i = 0; i < qty; i++) {
-    var myNode = document.createElement('div');
+  for (let i = 0; i < qty; i++) {
+    let myNode = document.createElement('div');
     myNode.className= 'box';
     myBoxes.appendChild(myNode);
   }
+})(12);
 
-}
-
-createBoxes(howMany);
