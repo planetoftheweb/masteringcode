502c947268fb3649d5805c34104d3b1e463f6553
Ray Villalobos <ray@planetoftheweb.com>
2017-09-29 17:33:19 -0400

    modified .gitignore file

---
+++ builds/masteringcode/js/script.js
@@ -0,0 +1,25 @@
+var data, DOMNode;
+var request = new XMLHttpRequest();
+
+DOMNode = document.querySelector('.artists');
+
+request.open('GET', 'js/data.json');
+
+request.onreadystatechange = function () {
+  if (request.status === 200 && request.readyState === 4) {
+    data = JSON.parse(request.responseText);
+
+    for (var item in data.artists) {
+      if (data.artists.hasOwnProperty(item)) {
+        var element = data.artists[item];
+        var listItem = document.createElement('div');
+        listItem.className = 'artist';
+        listItem.innerHTML = '<h4>' + element.name + '</h4>' + '<p>' + element.bio + '</p>';
+        DOMNode.appendChild(listItem);
+      }
+    }
+  }
+};
+
+request.send();
+//# sourceMappingURL=script.js.map
