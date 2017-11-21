459d9b985f0d3f57a329d252ff4275295bbee9ad
Ray Villalobos <ray@planetoftheweb.com>
2017-11-21 14:20:51 -0800

    finished promises

--- process/js/script.js
+++ process/js/script.js
@@ -1,16 +1,32 @@
-var artists;
+var artists, calendar;
 
 function loadData(url) {
+  return new Promise(function (resolve, reject) {
     var req = new XMLHttpRequest();
     req.open('GET', url);
-    req.onload = function() {
+    req.onload = function () {
       if (req.status == 200) {
-        artists = JSON.parse(req.response);
-        console.log(artists);
+        console.log('getting ' + url);
+        resolve(JSON.parse(req.response))
+      } else {
+        reject(Error('Problem loading data'))
       }
     } // onload callback
     req.send();
+  });
 }
 
-loadData('js/data_artists.json');
-console.log(artists);
+calendar = loadData('js/data_schedule.json');
+artists = loadData('js/data_artists.json');
+
+document.querySelector('.load-info')
+  .addEventListener('click', function () {
+    calendar.then(function (data) {
+      console.log(data);
+      return artists;
+    }, function (error) {
+      console.log('Problem', error);
+    }).then(function (otherData) {
+      console.log(otherData);
+    });
+  });
